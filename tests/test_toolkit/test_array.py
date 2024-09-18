baf8f0cf20b15cb1b60d3948626b622962c56ded
carefree0910 <syameimaru.saki@gmail.com>
2024-09-18 19:07:45 +0800

    ✅🗑️Unified `test_fast_concat_2d_axis0`

--- tests/test_toolkit/test_array.py
+++ tests/test_toolkit/test_array.py
@@ -15,23 +15,13 @@ def generate_arrays(dtype: np.dtype) -> List[np.ndarray]:
     ]
 
 
-def test_fast_concat_2d_axis0_f32():
-    for _ in range(10):
-        arrays = generate_arrays(np.float32)
-        np.testing.assert_allclose(
-            np.concatenate(arrays, axis=0),
-            fast_concat_2d_axis0(arrays),
-        )
-        dfs = [pd.DataFrame(a) for a in arrays]
-        assert pd.concat(dfs).equals(fast_concat_dfs_axis0(dfs))
-
-
-def test_fast_concat_2d_axis0_f64():
-    for _ in range(10):
-        arrays = generate_arrays(np.float64)
-        np.testing.assert_allclose(
-            np.concatenate(arrays, axis=0),
-            fast_concat_2d_axis0(arrays),
-        )
-        dfs = [pd.DataFrame(a) for a in arrays]
-        assert pd.concat(dfs).equals(fast_concat_dfs_axis0(dfs))
+def test_fast_concat_2d_axis0():
+    for dtype in [np.float32, np.float64]:
+        for _ in range(10):
+            arrays = generate_arrays(dtype)
+            np.testing.assert_allclose(
+                np.concatenate(arrays, axis=0),
+                fast_concat_2d_axis0(arrays),
+            )
+            dfs = [pd.DataFrame(a) for a in arrays]
+            assert pd.concat(dfs).equals(fast_concat_dfs_axis0(dfs))
