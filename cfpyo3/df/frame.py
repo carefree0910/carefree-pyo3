a5984e8622e733d30791614123db58dc0536cbf4
carefree0910 <syameimaru.saki@gmail.com>
2024-09-18 10:05:08 +0800

    ⚡️Delayed some imports

--- cfpyo3/df/frame.py
+++ cfpyo3/df/frame.py
@@ -1,7 +1,5 @@
-import numpy as np
-import pandas as pd
-
 from typing import Tuple
+from typing import TYPE_CHECKING
 from cfpyo3._rs.df import DataFrameF64
 from cfpyo3._rs.df import INDEX_CHAR_LEN
 from cfpyo3._rs.df.frame import new
@@ -10,6 +8,10 @@ from cfpyo3._rs.df.frame import shape
 from cfpyo3._rs.df.frame import index
 from cfpyo3._rs.df.frame import columns
 from cfpyo3._rs.df.frame import values
+
+if TYPE_CHECKING:
+    import numpy as np
+    import pandas as pd
 
 
 class DataFrame:
@@ -27,12 +29,16 @@ class DataFrame:
     def shape(self) -> Tuple[int, int]:
         return shape(self._df)
 
-    def rows(self, indices: np.ndarray) -> "DataFrame":
+    def rows(self, indices: "np.ndarray") -> "DataFrame":
+        import numpy as np
+
         if indices.dtype != np.int64:
             indices = indices.astype(np.int64)
         return DataFrame(rows(self._df, indices))
 
-    def to_pandas(self) -> pd.DataFrame:
+    def to_pandas(self) -> "pd.DataFrame":
+        import pandas as pd
+
         return pd.DataFrame(
             values(self._df),
             index=index(self._df),
@@ -41,7 +47,9 @@ class DataFrame:
         )
 
     @classmethod
-    def from_pandas(cls, df: pd.DataFrame) -> "DataFrame":
+    def from_pandas(cls, df: "pd.DataFrame") -> "DataFrame":
+        import numpy as np
+
         index = df.index.values
         columns = df.columns.values.astype(f"S{INDEX_CHAR_LEN}")
         values = df.values
--- cfpyo3/toolkit/array.py
+++ cfpyo3/toolkit/array.py
@@ -1,11 +1,15 @@
-import numpy as np
-
 from typing import List
+from typing import TYPE_CHECKING
 from cfpyo3._rs.toolkit.array import fast_concat_2d_axis0_f32
 from cfpyo3._rs.toolkit.array import fast_concat_2d_axis0_f64
 
+if TYPE_CHECKING:
+    import numpy as np
 
-def fast_concat_2d_axis0(arrays: List[np.ndarray]) -> np.ndarray:
+
+def fast_concat_2d_axis0(arrays: List["np.ndarray"]) -> "np.ndarray":
+    import numpy as np
+
     pivot = arrays[0]
     if pivot.dtype == np.float32:
         out = fast_concat_2d_axis0_f32(arrays)
