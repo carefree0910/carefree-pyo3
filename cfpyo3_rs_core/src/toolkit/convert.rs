4ecb9fd79dfe3e4d4ac4ce06f1fc2dc2b9ed7f19
carefree0910 <syameimaru.saki@gmail.com>
2024-09-22 09:39:13 +0800

    📝Added docstring for the `unsafe`s

--- cfpyo3_rs_core/src/toolkit/convert.rs
+++ cfpyo3_rs_core/src/toolkit/convert.rs
@@ -1,11 +1,24 @@
 use core::mem::size_of;
 
+/// get a bytes representation of <T> values, in a complete zero-copy way
+///
+/// # Safety
+///
+/// The caller must ensure that the `values` lives longer than the returned
+/// bytes representation, otherwise it will cause undefined behavior.
 #[inline]
 pub unsafe fn to_bytes<T: Sized>(values: &[T]) -> &[u8] {
     let nbytes = values.len() * size_of::<T>();
     unsafe { core::slice::from_raw_parts(values.as_ptr() as *mut u8, nbytes) }
 }
 
+/// convert bytes into <T> values, in a complete zero-copy way
+///
+/// # Safety
+///
+/// The caller must ensure that `bytes` is a valid slice of `T`, that is
+/// - the bytes are representing valid `T` values
+/// - the length of `bytes` is a multiple of the size of `T`
 #[inline]
 pub unsafe fn from_bytes<T: Sized>(bytes: Vec<u8>) -> Vec<T> {
     let values_len = bytes.len() / size_of::<T>();
