9b11d88647146b64ec0d556902b159ea55951f8e
carefree0910 <syameimaru.saki@gmail.com>
2024-09-22 14:22:53 +0800

    ✨Re-exports some stuffs

--- cfpyo3_rs_core/src/io.rs
+++ cfpyo3_rs_core/src/io.rs
@@ -1,1 +1,6 @@
 pub mod temporal;
+
+#[cfg(feature = "io-mem-redis")]
+pub use temporal::mem::fetchers::redis::RedisClient;
+#[cfg(feature = "io-mem-redis")]
+pub use temporal::mem::fetchers::redis::REDIS_KEY_NBYTES;
