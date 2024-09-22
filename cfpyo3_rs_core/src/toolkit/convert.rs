51116debe9d5e17d6db06f40d5791d73270d1dda
carefree0910 <syameimaru.saki@gmail.com>
2024-09-22 09:26:42 +0800

    🎨Slight refactor

--- cfpyo3_rs_core/src/toolkit/convert.rs
+++ cfpyo3_rs_core/src/toolkit/convert.rs
@@ -1,12 +1,14 @@
+use core::mem::size_of;
+
 #[inline]
 pub fn to_bytes<T: Sized>(values: &[T]) -> &[u8] {
-    let nbytes = values.len() * core::mem::size_of::<T>();
+    let nbytes = values.len() * size_of::<T>();
     unsafe { core::slice::from_raw_parts(values.as_ptr() as *mut u8, nbytes) }
 }
 
 #[inline]
 pub fn from_bytes<T: Sized>(bytes: Vec<u8>) -> Vec<T> {
-    let values_len = bytes.len() / core::mem::size_of::<T>();
+    let values_len = bytes.len() / size_of::<T>();
     unsafe {
         let vec: Vec<T> = Vec::from_raw_parts(bytes.as_ptr() as *mut T, values_len, values_len);
         core::mem::forget(bytes);
