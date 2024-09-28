af3e8b6bbe548f8e63624054f28f411ad4210829
carefree0910 <syameimaru.saki@gmail.com>
2024-09-28 15:32:58 +0800

    ✅Added `searchsorted` bench

--- cfpyo3_rs_core/benches/array.rs
+++ cfpyo3_rs_core/benches/array.rs
@@ -1,8 +1,8 @@
 use cfpyo3_core::toolkit::array::*;
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use ndarray_rand::rand_distr::Uniform;
 use ndarray_rand::RandomExt;
-use numpy::ndarray::Array2;
+use ndarray_rand::{rand::seq::SliceRandom, rand_distr::Uniform};
+use numpy::ndarray::{Array1, Array2};
 
 macro_rules! bench_mean_axis1 {
     ($c:expr, $multiplier:expr, $nthreads:expr, $a32:expr, $a64:expr) => {{
@@ -64,5 +64,24 @@ fn bench_axis1_ops(c: &mut Criterion) {
     bench_corr_axis1_full!(c, 8);
 }
 
-criterion_group!(benches, bench_axis1_ops);
+fn bench_searchsorted(c: &mut Criterion) {
+    let total = 5000;
+    let array_i64 = Array1::<i64>::from_iter(0..total);
+    let array_i64 = &array_i64.view();
+    for amount in [2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048].iter() {
+        let random_picked: Vec<i64> = array_i64
+            .as_slice()
+            .unwrap()
+            .choose_multiple(&mut rand::thread_rng(), *amount)
+            .cloned()
+            .collect();
+        let random_picked = Array1::<i64>::from(random_picked);
+        let random_picked = &random_picked.view();
+        c.bench_function(&format!("searchsorted ({} / {})", amount, total), |b| {
+            b.iter(|| batch_searchsorted(black_box(array_i64), black_box(random_picked)))
+        });
+    }
+}
+
+criterion_group!(benches, bench_axis1_ops, bench_searchsorted);
 criterion_main!(benches);
