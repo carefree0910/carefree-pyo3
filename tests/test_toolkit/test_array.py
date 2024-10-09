18d260d5d4a76fd4a9d96672c4263ac05421920d
carefree0910 <syameimaru.saki@gmail.com>
2024-10-09 15:45:26 +0800

    ✅Introduced `test_coeff_axis1`

--- tests/test_toolkit/test_array.py
+++ tests/test_toolkit/test_array.py
@@ -6,8 +6,10 @@ import pandas as pd
 from typing import List
 from cfpyo3.toolkit.array import corr_axis1
 from cfpyo3.toolkit.array import mean_axis1
+from cfpyo3.toolkit.array import coeff_axis1
 from cfpyo3.toolkit.array import masked_mean_axis1
 from cfpyo3.toolkit.array import masked_corr_axis1
+from cfpyo3.toolkit.array import masked_coeff_axis1
 from cfpyo3.toolkit.array import fast_concat_2d_axis0
 from cfpyo3.toolkit.array import fast_concat_dfs_axis0
 
@@ -70,3 +72,44 @@ def test_corr_axis1():
             valid_mask = np.isfinite(a) & np.isfinite(b)
             assert_allclose(batch_corr(a, b), corr_axis1(a, b))
             assert_allclose(batch_corr(a, b), masked_corr_axis1(a, b, valid_mask))
+
+
+def lstsq(q: float, valid: np.ndarray, a0: np.ndarray, a1: np.ndarray) -> np.ndarray:
+    coefficients = np.zeros([a0.shape[0], 2], a0.dtype)
+    for t, t_labels in enumerate(a1):
+        t_valid = valid[t]
+        t_labels = t_labels[t_valid]
+        t_signal = a0[t][t_valid]
+        t_signal_med = np.median(t_signal)
+        t_signal_mad = np.median(np.abs(t_signal - t_signal_med))
+        t_signal = np.clip(
+            t_signal,
+            t_signal_med - 100.0 * t_signal_mad,
+            t_signal_med + 100.0 * t_signal_mad,
+        )
+        t_signal = (t_signal - np.mean(t_signal)) / np.std(t_signal)
+        if q > 0.0:
+            floor_mask = t_signal <= np.quantile(t_signal, q)
+            ceil_mask = t_signal >= np.quantile(t_signal, 1.0 - q)
+            picked_mask = floor_mask | ceil_mask
+            t_labels = t_labels[picked_mask]
+            t_signal = t_signal[picked_mask]
+        t_signal = np.column_stack((t_signal, np.ones_like(t_signal)))
+        w, b = np.linalg.lstsq(t_signal, t_labels, rcond=None)[0]
+        coefficients[t] = [w, b]
+    return coefficients
+
+
+def test_coeff_axis1():
+    for dtype in [np.float32, np.float64]:
+        for q in [0.0, 0.1]:
+            a0 = generate_array(dtype)
+            a1 = generate_array(dtype)
+            valid_mask = np.isfinite(a0) & np.isfinite(a1)
+            gt_w, gt_b = lstsq(q, valid_mask, a0, a1).T
+            w, b = coeff_axis1(a0, a1, q)
+            assert_allclose(gt_w, w)
+            assert_allclose(gt_b, b)
+            w, b = masked_coeff_axis1(a0, a1, valid_mask, q)
+            assert_allclose(gt_w, w)
+            assert_allclose(gt_b, b)
