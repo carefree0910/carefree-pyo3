eeadaa2c3c44829c0fbabe4fed53dd8fda94ae52
carefree0910 <syameimaru.saki@gmail.com>
2024-10-19 11:09:28 +0800

    💥🗑️Cleaned up `batch_*` operations
    it is better to let users to decide how to make batch processes.

--- cfpyo3_rs_core/src/io/temporal/source.rs
+++ cfpyo3_rs_core/src/io/temporal/source.rs
@@ -1,7 +1,6 @@
 use crate::{df::DataFrame, toolkit::array::AFloat};
 use anyhow::Result;
-use itertools::{izip, Itertools};
-use std::{future::Future, iter::zip};
+use std::future::Future;
 
 #[cfg(feature = "io-source-opendal")]
 pub mod s3;
@@ -11,27 +10,4 @@ pub trait Source<T: AFloat> {
     fn read(&self, date: &str, key: &str) -> impl Future<Output = Result<DataFrame<T>>>;
     /// write data of specific `date` and `key` to source
     fn write(&self, date: &str, key: &str, df: &DataFrame<T>) -> impl Future<Output = Result<()>>;
-    /// batch version of `read`
-    fn batch_read(
-        &self,
-        dates: &[&str],
-        keys: &[&str],
-    ) -> impl Future<Output = Result<Vec<DataFrame<T>>>> {
-        let futures = zip(dates, keys)
-            .map(|(date, key)| self.read(date, key))
-            .collect_vec();
-        futures::future::try_join_all(futures)
-    }
-    /// batch version of `write`
-    fn batch_write(
-        &self,
-        dates: &[&str],
-        keys: &[&str],
-        dfs: &[&DataFrame<T>],
-    ) -> impl Future<Output = Result<Vec<()>>> {
-        let futures = izip!(dates, keys, dfs)
-            .map(|(date, key, df)| self.write(date, key, df))
-            .collect_vec();
-        futures::future::try_join_all(futures)
-    }
 }
